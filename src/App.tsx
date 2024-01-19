af2b311efb65e670f819d163bb4f3348356e67be
Rei Komatsuzaki <kokky@ReinoMacBook-Air.local>
2024-01-20 00:14:50 +0900

    build

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/komatsuzaki-s-portfolio/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
